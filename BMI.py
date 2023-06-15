8a7b6ffc2504c114896ec4319256b7b94147417a
Apirak Fakin <136418361+Apirak19@users.noreply.github.com>
2023-06-15 11:44:10 +0700

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,23 @@
+print("This is BMI calculator")
+weight = int(input("your weight (kg):"))
+height = int(input("your height (cm):"))
+
+BMI = weight/((height/100)**2)
+# if BMI<19:
+#     BMIR = "UnderWeight"
+# elif 19<BMI<25:
+#     BMIR = "NormalWight"
+# elif 25<BMI<30:
+#     BMIR = "OverWeight"
+if BMI<30:
+    if BMI<19:
+        BMIR = "UnderWeight"
+    elif 19<BMI<25:
+        BMIR = "NormalWight"
+    elif 25<BMI<30:
+        BMIR = "OverWeight"
+else:
+    BMIR = "... Go see a doctor ASAP!!"
+
+print("Your BMI: ", BMI)
+print("You are ", BMIR)
